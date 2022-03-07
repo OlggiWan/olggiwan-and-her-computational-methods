11837ef48859840a58402118d4321bfa987f8f8b
OlggiWan <99152894+OlggiWan@users.noreply.github.com>
2022-03-07 02:23:25 -0500

    Update and rename Lab3 to Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -3,6 +3,8 @@ import numpy as np
 
 # This section of the lab deals with the derivative problem. (Exercise 4.3)
 
+print("The following program calculates the derivative of a function, then tests its accuracy as sigma becomes increasingly smaller.")
+
 delta = np.array ([1e-2, 1e-4, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-12, 1e-14])
 x = 1
 
@@ -11,10 +13,11 @@ def limit (x):
 
 derivative = ((limit(x+delta) - limit(x))/delta)
 
-print(derivative)
+print("These are the derivative values: ",derivative)
 
 plt.xscale("log")
 plt.ylim([0.998, 1.002])
+plt.xlabel("Delta")
+plt.ylabel("Derivative")
 _=plt.plot(delta,derivative)
 plt.show()
-
