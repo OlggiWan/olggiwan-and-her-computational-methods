658b8b83bbbe557ed5a4d22b925325e8a6638579
OlggiWan <99152894+OlggiWan@users.noreply.github.com>
2022-03-20 03:17:49 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -8,3 +8,11 @@ g = 9.8
 h = 100
 time = (((2*h)**0.5)/g)
 print(time)
+
+"""
+The second part asks: A spaceship travels from Earth in a straight line at relativistic speed v to another planet x light years away. Write a program to
+ask the user for the value of x and the speed v as a fraction of the speed of light c, then print out the time in years that the spaceship takes to reach
+its destination (a) in the rest frame of an observer on Earth and (b) as perceived by a passenger on board the ship. Use your program to calculate the 
+answers for a planet 10 light years away with v = 0.99c.
+"""
+
