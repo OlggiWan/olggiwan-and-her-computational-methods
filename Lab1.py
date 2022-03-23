c1883fda439c88ba52802ae0e39b12991e1290dd
OlggiWan <99152894+OlggiWan@users.noreply.github.com>
2022-03-23 00:58:54 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -15,4 +15,16 @@ ask the user for the value of x and the speed v as a fraction of the speed of li
 its destination (a) in the rest frame of an observer on Earth and (b) as perceived by a passenger on board the ship. Use your program to calculate the 
 answers for a planet 10 light years away with v = 0.99c.
 """
+import numpy as np
+
+ly = 0.99
+v = 10
+gamma = 1/((1-v**2)**0.5)
+dilation = ly/gamma
+earth = ly/v
+spaceship = dilation/v
+
+print("This is time, in years, that passes on the rest frame of the observer on Earth", earth)
+print("This is the time, in years, that the spaceship takes to get to its destination, as perceived by as passenger on board the ship ",spaceship)
+
 
